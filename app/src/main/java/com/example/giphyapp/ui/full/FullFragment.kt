427843014501e9e2d8c7cf16f32542c4de8483eb
SurenOhanesian <115486102+SurenOhanesian@users.noreply.github.com>
2023-09-01 14:39:46 +0300

    init full fragment

--- app/src/main/java/com/example/giphyapp/ui/full/FullFragment.kt
+++ app/src/main/java/com/example/giphyapp/ui/full/FullFragment.kt
@@ -1,9 +1,23 @@
 package com.example.giphyapp.ui.full
 
-import com.example.giphyapp.databinding.FragmentHomeBinding
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.giphyapp.databinding.FragmentFullBinding
 import com.example.giphyapp.ui.BaseFragment
 
-class FullFragment : BaseFragment<FragmentHomeBinding>() {
+class FullFragment : BaseFragment<FragmentFullBinding>() {
+    private val args: FullFragmentArgs by navArgs()
 
-    override fun inflateBinding() = FragmentHomeBinding.inflate(layoutInflater)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.back.setOnClickListener { popBackStack() }
+        Glide.with(requireContext())
+            .load(args.link)
+            .into(binding.gif)
+    }
+
+    override fun inflateBinding() = FragmentFullBinding.inflate(layoutInflater)
 }
